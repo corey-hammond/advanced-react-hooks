2237e3c4edc0b2d4bafaf79f276b496a3354eb62
Corey Hammond <coreyjhammond@gmail.com>
2021-10-08 11:44:40 -0700

    completed exercise 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,16 +3,74 @@
 
 import * as React from 'react'
 
-function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+// SIMPLE USEREDUCER EXAMPLE
+// const countReducer = (state, newState) => newState
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+// function Counter({initialCount = 0, step = 1}) {
+//   const [count, setCount] = React.useReducer(countReducer, initialCount)
+//   const increment = () => setCount(count + step)
+//   return <button onClick={increment}>{count}</button>
+// }
+
+// EXAMPLE OF USEREDUCER WITH DIFFERENT ACTION PASSED
+// const countReducer = (count, change) => count + change
+
+// function Counter({initialCount = 0, step = 1}) {
+//   const [count, changeCount] = React.useReducer(countReducer, initialCount)
+//   const increment = () => changeCount(step)
+//   return <button onClick={increment}>{count}</button>
+// }
+
+// EXAMPLE OF USEREDUCER WITH OBJECT PASSED
+// const countReducer = (state, action) => ({...state, ...action})
+
+// function Counter({initialCount = 0, step = 1}) {
+//   const [state, setState] = React.useReducer(countReducer, {
+//     count: initialCount,
+//   })
+//   const {count} = state
+//   const increment = () => setState({count: count + step})
+//   return <button onClick={increment}>{count}</button>
+// }
+
+// EXAMPLE OF USEREDUCER AND SETSTATE WITH AN OBJECT OR FUNCTION
+// const countReducer = (state, action) => ({
+//   ...state,
+//   ...(typeof action === 'function' ? action(state) : action),
+// })
+
+// function Counter({initialCount = 0, step = 1}) {
+//   const [state, setState] = React.useReducer(countReducer, {
+//     count: initialCount,
+//   })
+//   const {count} = state
+//   const increment = () =>
+//     setState(currentState => ({count: currentState.count + step}))
+//   return <button onClick={increment}>{count}</button>
+// }
+
+// EXAMPLE OF CONVENTIONAL SWITCH STATEMENT
+const countReducer = (state, action) => {
+  const {type, payload} = action
+  switch (type) {
+    case 'INCREMENT': {
+      return {
+        ...state,
+        count: state.count + payload,
+      }
+    }
+    default: {
+      throw new Error(`Unsupported action type: ${type}`)
+    }
+  }
+}
+
+function Counter({initialCount = 0, step = 2}) {
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const {count} = state
+  const increment = () => dispatch({type: 'INCREMENT', payload: step})
   return <button onClick={increment}>{count}</button>
 }
 
